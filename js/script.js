2ce6a5d91e550d9828831cd84d5a692b4ece73b1
aipa <gundamgundam777@gmail.com>
2019-02-11 22:53:48 +0900

    [modify] デザイン修正.

---
+++ js/script.js
@@ -0,0 +1,40 @@
+//Vueのインスタンスの定義
+
+var app = new Vue({
+  el: "#app",　// vueのインスタンスが紐づくDOM要素ののセレクタ
+  data:{　　　// ここで定義した値がv-model="hoge"や{{hoge}}の初期値に反映される
+    url: 'https://159oyrsgc0.execute-api.ap-northeast-1.amazonaws.com/lt_test', // v-model="url"の初期値
+    selected_movie: {},
+    param: "{}",              // v-model="param"の初期値
+    results: []               // v-model="result"の初期値
+  },
+  mounted() {　  //v-on:click="hoge"などのイベントに紐づく関数定義
+    // Axiosを使ったAJAX
+    // リクエスト時のオプションの定義
+    config = {
+      headers:{
+        'X-Requested-With': 'XMLHttpRequest',
+        'Content-Type':'application/json',
+        'Access-Control-Allow-Origin': 'http://localhost:8000'
+      },
+      withCredentials: true,
+    }
+    // vueでバインドされた値はmethodの中ではthisで取得できる
+    param = JSON.parse(this.param)
+
+    // Ajaxリクエスト
+    axios.get(this.url, param, config)
+      .then(function(res) {
+        // vueにバインドされている値を書き換えると表示に反映される
+        app.results = JSON.parse(res.data['body'])[0]['results'];
+        console.log(app.results);
+      })
+      .catch(function(res){
+        // vueにバインドされている値を書き換えると表示に反映される
+　　　  app.result = res.data;
+        console.log(res);
+      });
+  }
+});
+
+app;
